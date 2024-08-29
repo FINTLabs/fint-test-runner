b1ad0496cdb8a67ae6d031a901acb9dc2d3a21a2
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2024-08-29 11:57:42 +0200

    upgrade deprecated security config

--- src/main/java/no/fint/testrunner/config/SecurityConfiguration.java
+++ src/main/java/no/fint/testrunner/config/SecurityConfiguration.java
@@ -1,19 +1,22 @@
 package no.fint.testrunner.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/**")
-                .authenticated();
+                .authorizeRequests(a -> a
+                        .anyRequest()
+                        .authenticated()
+                )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        return http.build();
     }
 }
