66958c48cb03c2fa0413cea221ef7bc55ec9a0a8
Hknots <hknots99@gmail.com>
2024-08-30 09:59:12 +0200

    Revert "permit all requests but disable ingress"
    This reverts commit 98d26e9c8d57f30b547509d37a04eb1d89184ee7.

--- src/main/java/no/fint/config/SecurityConfiguration.java
+++ src/main/java/no/fint/config/SecurityConfiguration.java
@@ -3,21 +3,21 @@ package no.fint.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeRequests(request -> request
+                .authorizeRequests(a -> a
                         .anyRequest()
                         .authenticated()
-                )
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-                .build();
+                ).build();
     }
+
 
 }
